2a67d2f061ce74dedabec8d61ea1b29e85056310
Stonewall1 <impression97p@gmail.com>
2022-11-20 22:47:55 +0300

    homepage front(if login/else welcome)

--- src/main/java/com/autoservice/web/controller/AutoserviceController.java
+++ src/main/java/com/autoservice/web/controller/AutoserviceController.java
@@ -1,15 +1,21 @@
 package com.autoservice.web.controller;
 
+import com.autoservice.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
 
 @Controller
 @RequestMapping("/")
 public class AutoserviceController {
 
     @GetMapping
-    public String homepage(){
+    public String homepage(Model model , HttpSession session){
+        model.addAttribute("currentUser" , (User) session.getAttribute("currentUser"));
         return "homepage";
     }
 }
